bc47741ca9d821fb232f2934afa42b3a74dbc861
Andrew Poland <andrew@aserto.com>
2023-04-21 13:52:30 -0400

    remove registry secret

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-IMAGE_PULL_SECRET=$(vault kv get -field=DOCKER_CONFIG_JSON kv/github)
-GHCR_TOKEN=$(vault kv get -field=READ_WRITE_TOKEN kv/github)
 VAULT_ADDR=https://vault.eng.aserto.com
 vault login --no-print --method=github
+
+GHCR_TOKEN=$(vault kv get -field=READ_WRITE_TOKEN kv/github)
 
 
 # trap ctrl-c and call ctrl_c()
@@ -21,7 +21,7 @@ cleanup
 sleep 5
 
 echo ... Deploying helm chart ...
-helm install aserto1 aserto --set global.sidecar.ghcrToken="$GHCR_TOKEN",imagePullSecret="$IMAGE_PULL_SECRET"
+helm install aserto1 aserto --set global.sidecar.ghcrToken="$GHCR_TOKEN"
 
 echo ... Waiting for services to become ready ...
 POSTGRES_POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/name=postgresql,app.kubernetes.io/instance=aserto1" -o jsonpath="{.items[0].metadata.name}")
@@ -45,14 +45,6 @@ kubectl --namespace default port-forward $AUTHORIZER_POD_NAME 8383:8383 &
 kubectl --namespace default port-forward $AUTHORIZER_POD_NAME 8282:8282 &
 #kubectl --namespace default port-forward $CONSOLE_POD_NAME 8080:8080 &
 kubectl --namespace default port-forward $NGINX_POD_NAME 8080:8080 &
-
-
-sleep 5
-echo ... Creating directory tenant ...
-grpcurl --insecure -H "authorization: basic c96fe48da0735cf77fdba50134f52c43" \
--d '{"tenant": {"id": "07f3e43c-a734-11ed-a7a0-002270b772a6", "name": "test"}}' \
-localhost:9292 aserto.directory.store.v2.Store.CreateTenant
-
 
 echo
 echo '*** Console is listening on http://localhost:8080 ***'
